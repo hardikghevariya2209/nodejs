41407233aba46376579cc779a461fe7ea8e69eb0
Hardik Ghevariya <hardikghevariya.vision@gmail.com>
2023-03-03 18:08:43 +0530

    error fixed"

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 const http = require('http')
-const server = http.createServer((req, res) => {
-    if (req.url === "/") {
-        res.writeHead(200, { "Content-Type": "text/plain" });
-        res.end("Home page");
-    } else if (req.url === "/slow-page") {
-        res.writeHead(200, { "Content-Type": "text/plain" });
-        res.end("Slow Page");
-    }
-});
 
-server.listen(8000, () => console.log("Server is running on port 8000"));
+http.createServer((req, res) => {
+    res.writeHead(200)
+    res.end('Write')
+}).listen(3000)
