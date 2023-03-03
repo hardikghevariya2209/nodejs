b4beeb2ca8d3fd5facbce2a26c81c8759060630b
Hardik Ghevariya <hardikghevariya.vision@gmail.com>
2023-03-03 18:14:26 +0530

    error fixed"

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const http = require('http')
 
 http.createServer((req, res) => {
-    res.writeHead(200)
+    res.writeHead(200, { 'Access-Control-Allow-Origin': '*' })
     res.end('Write')
 }).listen(3000)
