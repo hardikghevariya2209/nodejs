2bd173d8e5eb2912389c1beef683f90524aaa846
Hardik Ghevariya <hardikghevariya.vision@gmail.com>
2023-03-03 17:26:30 +0530

    error fixed"

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
-const getUserMedia = require('getusermedia')
-// const http = require('http')
-// const server = http.createServer((req, res) => {
-//     if (req.url === "/") {
-//         res.writeHead(200, { "Content-Type": "text/plain" });
-//         res.end("Home page");
-//     } else if (req.url === "/slow-page") {
-//         res.writeHead(200, { "Content-Type": "text/plain" });
-//         res.end("Slow Page");
-//     }
-// });
+const http = require('http')
+const server = http.createServer((req, res) => {
+    if (req.url === "/") {
+        res.writeHead(200, { "Content-Type": "text/plain" });
+        res.end("Home page");
+    } else if (req.url === "/slow-page") {
+        res.writeHead(200, { "Content-Type": "text/plain" });
+        res.end("Slow Page");
+    }
+});
 
-// server.listen(8000, () => console.log("Server is running on port 8000"));
-
-getUserMedia({ audio: true }, function (err, stream) {
-    console.log(err, stream);
-})
+server.listen(8000, () => console.log("Server is running on port 8000"));
